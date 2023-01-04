95db5b4afca83842c34fe57bf043dc473fd42ca6
James Ocana <ojam1996@gmail.com>
2023-01-04 14:44:37 -0500

    fixed dropdown styling on navbar

--- src/Pages/MemberDashboard/MemberDashboard.js
+++ src/Pages/MemberDashboard/MemberDashboard.js
@@ -4,8 +4,8 @@ import NavBar from "../../Components/NavBar/NavBar";
 const MemberDashboard = ({props}) => {
     return (
         <div>
-            <h1>MemberDashboard Page</h1>
             <NavBar/>
+            <h1>MemberDashboard Page</h1>
         </div>
     )
 } 

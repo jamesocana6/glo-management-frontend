eb74f0c2f6f07a73472e4f18b6d8f5fd01dcacb8
Peter Lam <plam1216@gmail.com>
2023-01-04 14:39:04 -0500

    created Coach Filter Form

--- src/Pages/FindCoach/Filters/Filters.js
+++ src/Pages/FindCoach/Filters/Filters.js
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Filters = ({props}) => {
+const Filters = ({ props }) => {
+    const [filter, setFilter] = useState({
+        position: '',
+        company: '',
+        location: '',
+    })
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+        setFilter({ ...filter, [event.target.name]: event.target.value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("filter", filter)
+    }
+
     return (
-        <div>
-            <h1>Filters Component</h1>
+        <div className="row">
+            <form className="col s12" onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="input-field col s3">
+                        <i className="material-icons prefix">account_circle</i>
+                        <input
+                            id="icon_position"
+                            type="text"
+                            className="validate"
+                            name="position" onChange={handleChange} />
+                        <label for="icon_position">Position</label>
+                    </div>
+
+                    <div className="input-field col s3">
+                        <i className="material-icons prefix">business</i>
+                        <input
+                            id="icon_company"
+                            type="text"
+                            className="validate"
+                            name="company"
+                            onChange={handleChange} />
+                        <label for="icon_company">Company</label>
+                    </div>
+
+                    <div className="input-field col s3">
+                        <i className="material-icons prefix">location_city</i>
+                        <input
+                            id="icon_location"
+                            type="text"
+                            className="validate"
+                            name="location"
+                            onChange={handleChange} />
+                        <label for="icon_location">Location</label>
+                    </div>
+
+                    <input type="submit" className="btn" value="Search" />
+                </div>
+            </form>
         </div>
     )
-} 
+}
 
 export default Filters;

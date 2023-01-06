86835ca38ffd29a6a80a0dbd6aa0adf95ae33641
Peter Lam <plam1216@gmail.com>
2023-01-06 14:37:58 -0500

    setup coach cards with mock data

--- src/Pages/FindCoach/CoachCard/CoachCard.js
+++ src/Pages/FindCoach/CoachCard/CoachCard.js
@@ -1,23 +1,38 @@
 import React from "react";
 
 const CoachCard = ({ props }) => {
+    // mock data
+    const people = [
+        {
+            name: 'Person 1',
+            position: 'Frontend Engineer',
+            company: 'Apple',
+            location: 'California',
+        },
+        {
+            name: 'Person 2',
+            position: 'Backend Engineer',
+            company: 'Microsoft',
+            location: 'Oregon',
+        }
+    ]
     return (
-        <div className="row">
-            <div className="col s12 m7">
-                <div className="card">
-                    <div className="card-image">
-                        <img src="https://imgur.com/JBZZxqT.jpg" />
-                    </div>
-                    <div class="card-content">
-                        <p>Name: Person 1</p>
-                        <p>Position: Position 1</p>
-                        <p>Company: Company 1</p>
-                        <p>Location: Location 1</p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">Connect with ...!</a>
-                    </div>
-                </div>
+        <div className="container-fluid">
+            <div className="row">
+                {people.map(person => {
+                    return (
+                        <div className="card m-2 " style={{ width: '18rem' }}>
+                            <img src="https://imgur.com/JBZZxqT.jpg" className="card-img-top" alt="image" />
+                            <div className="card-body" style={{ textAlign: 'left' }}>
+                                <h5 className="card-title">{person.name}</h5>
+                                <p className="card-text">{person.position}</p>
+                                <p className="card-text">{person.company}</p>
+                                <p className="card-text">{person.location}</p>
+                            </div>
+                            <button className="btn btn-secondary">Modal?..</button>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )

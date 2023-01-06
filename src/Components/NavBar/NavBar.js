d8fcac24383bc97a1365e3973b1df29dbef847e4
James Ocana <ojam1996@gmail.com>
2023-01-06 15:22:01 -0500

    styled navbar with org colors

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,38 +1,52 @@
 import React from "react";
+import "./style.css";
 
 const NavBar = ({ props }) => {
     return (
-        <>
-            <ul id="dropdown1" className="dropdown-content">
-                <li><a href="#!">one</a></li>
-                <li><a href="#!">two</a></li>
-                <li className="divider"></li>
-                <li><a href="#!">three</a></li>
-            </ul>
-            <ul id="dropdown2" className="dropdown-content">
-                <li><a href="#!">a</a></li>
-                <li><a href="#!">b</a></li>
-                <li className="divider"></li>
-                <li><a href="#!">c</a></li>
-            </ul>
-            <ul id="dropdown3" className="dropdown-content">
-                <li><a href="#!">1</a></li>
-                <li><a href="#!">2</a></li>
-                <li className="divider"></li>
-                <li><a href="#!">3</a></li>
-            </ul>
-            <nav>
-                <div className="nav-wrapper">
-                    <a href="#!" className="brand-logo left">Logo</a>
-                    <ul className="right">
-                        <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">My Chapter<i className="material-icons right">arrow_drop_down</i></a></li>
-                        <li><a className="dropdown-trigger" href="#!" data-target="dropdown2">Nationals<i className="material-icons right">arrow_drop_down</i></a></li>
-                        <li><a className="dropdown-trigger" href="#!" data-target="dropdown3">All Events<i className="material-icons right">arrow_drop_down</i></a></li>
-                        <li><a href="/">Profile</a></li>
-                    </ul>
+        <div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <div>
+                        <ul className="navbar-nav justify-content-end">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    My Chapter
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Nationals
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    All Events
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Profile</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
-        </>
+        </div>
     )
 }
 

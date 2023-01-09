5ec866c19c22a59382e9533f2d33220ddc658ed3
Peter Lam <plam1216@gmail.com>
2023-01-09 14:54:14 -0500

    add coach modal

--- src/Pages/FindCoach/CoachCard/CoachCard.js
+++ src/Pages/FindCoach/CoachCard/CoachCard.js
@@ -1,35 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import CoachModal from "../CoachModal/CoachModal";
 
 const CoachCard = ({ props }) => {
     // mock data
-    const people = [
-        {
-            name: 'Person 1',
-            position: 'Frontend Engineer',
-            company: 'Apple',
-            location: 'California',
-        },
-        {
-            name: 'Person 2',
-            position: 'Backend Engineer',
-            company: 'Microsoft',
-            location: 'Oregon',
-        }
-    ]
+    const [people, setPeople] = useState(
+        [
+            {
+                name: 'Person 1',
+                position: 'Frontend Engineer',
+                company: 'Apple',
+                location: 'California',
+                tags: ['Entrepreneur', 'Python']
+            },
+            {
+                name: 'Person 2',
+                position: 'Backend Engineer',
+                company: 'Microsoft',
+                location: 'Oregon',
+                tags: ['React', 'React Native']
+            }
+        ]
+    )
+
+    const [cardId, setCardId] = useState(0)
+
+    const handleClick = (event) => {
+        setCardId(event.target.closest("div").getAttribute("cardId"))
+    }
+
     return (
         <div className="container-fluid">
             <div className="row">
-                {people.map(person => {
+                {people.map((person, index) => {
                     return (
-                        <div className="card m-2 " style={{ width: '18rem' }}>
-                            <img src="https://imgur.com/JBZZxqT.jpg" className="card-img-top" alt="image" />
+                        <div className="card m-2" cardId={index} style={{ width: '18rem' }}>
+                            <img className="card-img-top" src="https://imgur.com/JBZZxqT.jpg" alt="placeholder" />
                             <div className="card-body" style={{ textAlign: 'left' }}>
-                                <h5 className="card-title">{person.name}</h5>
-                                <p className="card-text">{person.position}</p>
-                                <p className="card-text">{person.company}</p>
-                                <p className="card-text">{person.location}</p>
+                                <h5 className="card-title">{person.name}
+                                    <p className="card-text">{person.position}</p>
+                                    <p className="card-text">{person.company}</p>
+                                    <p className="card-text">{person.location}</p>
+                                </h5>
                             </div>
-                            <button className="btn btn-secondary">Modal?..</button>
+                            <button className="btn btn-secondary m-3" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={handleClick}>Open Modal</button>
+
+                            <CoachModal
+                                people={people}
+                                cardId={cardId}
+                            />
                         </div>
                     )
                 })}

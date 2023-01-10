aa6caff2de1ece933a5496248666ec6acd868838
James Ocana <ojam1996@gmail.com>
2023-01-10 13:27:49 -0500

    banner height fixed

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = ({ props }) => {
     return (
-        <div className="container" style={{height: 300, contain: "content", marginTop: 20, marginBottom: 20,}} >
+        <div className="container" style={{height: 250, contain: "content", marginTop: 20, marginBottom: 20,}} >
             <img src="https://imgur.com/JBZZxqT.jpg" alt="picture"/>
         </div>
     )

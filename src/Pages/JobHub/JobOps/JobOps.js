c96e724980d1c4ab02676660b95a8d09b079306d
James Ocana <ojam1996@gmail.com>
2023-01-10 14:34:03 -0500

    add a job links to correct route

--- src/Pages/JobHub/JobOps/JobOps.js
+++ src/Pages/JobHub/JobOps/JobOps.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const JobOps = ({ props }) => {
     return (
-        <>
-            <div>
+        <div className="container">
+            <div className="row">
+            <div className="col-9 text-start">
                 <h3>Job Opportunities</h3>
+            </div>
+            <div className="col text-end">
+                <Link to={"/jobhub/add"}>Add a Job</Link>
+            </div>
             </div>
             <p>
                 Title @ Company 1 location <br></br>
@@ -12,7 +18,7 @@ const JobOps = ({ props }) => {
                 Add a Job (link)<br></br>
                 See all job Opportunities<br></br>
             </p>
-        </>
+        </div>
     )
 }
 

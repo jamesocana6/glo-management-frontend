b1e94825bc94b057b66679daf400430aba00ea56
Peter Lam <plam1216@gmail.com>
2023-01-10 14:49:43 -0500

    style MemberDashboard components

--- src/Pages/MemberDashboard/MemberDashboard.js
+++ src/Pages/MemberDashboard/MemberDashboard.js
@@ -10,28 +10,42 @@ const MemberDashboard = ({ props }) => {
     return (
         <>
             <div>
-               <NavBar/>
-               <h1>MemberDashboard Page</h1>
-                           
-            </div>
-            <p>
+                <NavBar />
+                <h1>MemberDashboard Page</h1>
                 Hi First Name!
-                <MemberInfo />
-            </p>
-            <p>
-                <Announcements />
-                {/* props = chapter */}
-            </p>
-            <p>
-                National Resources<br></br>
-                <Link to="../findcoach">Find a Coach</Link><br></br>
-                <Link to="../jobhub">Job Hub - Opps & Resources</Link><br></br>
-                Support other Sister's Businesses<br></br>
-            </p>
-            <p>
-                <Announcements />
-                {/* props = national */}
-            </p>
+
+            </div>
+
+            <div className="container-fluid">
+                <div className="row m-3" style={{ height: '17rem' }}>
+                    <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
+                        <MemberInfo />
+                    </div>
+
+                    <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
+                        <Announcements />
+                        {/* props = chapter */}
+                    </div>
+                </div>
+
+                <div className="row m-3" style={{ height: '17rem' }}>
+                    <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
+                        <p>
+                            <h1>National Resources</h1>
+                            <Link to="../findcoach">Find a Coach</Link><br></br>
+                            <Link to="../jobhub">Job Hub - Opps & Resources</Link><br></br>
+                            Support other Sister's Businesses<br></br>
+                        </p>
+                    </div>
+
+                    <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
+                        <p>
+                            <Announcements />
+                            {/* props = national */}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/Pages/MemberDashboard/MemberInfo/MemberInfo.js
+++ src/Pages/MemberDashboard/MemberInfo/MemberInfo.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MemberInfo = ({ props }) => {
+    // mock info, this should come from props
+    const [memberInfo, setMemberInfo] = useState(
+        [
+            {
+                crossingSchool: 'NYU',
+                crossingClass: 'Beta',
+                currentSchool: 'N/A',
+                currentPosition: 'N/A',
+                previousPosition: 'N/A',
+            },
+        ]
+    )
+
+
     return (
         <div>
-            <h1>MemberInfo Component</h1>
-            Your crossing School & Class<br></br>
-            Your Current School (if applicable)<br></br>
-            Current Position:<br></br>
-            Previous Position():<br></br>
+            <h1>MemberInfo</h1>
+            {memberInfo.map((member) => {
+                return (
+                    <div>
+                        <p>
+                            Crossing School: {member.crossingSchool}
+                        </p>
+                        <p>
+                            Crossing Class: {member.crossingClass}
+                        </p>
+                        <p>
+                            Current School: {member.currentSchool}
+                        </p>
+                        <p>
+                            Current Position: {member.currentPosition}
+                        </p>
+                        <p>
+                            Previous Position: {member.previousPosition}
+                        </p>
+                    </div>
+                )
+            })}
         </div>
     )
 }

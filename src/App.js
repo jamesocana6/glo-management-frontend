b673186fb9f26274e60f9d80943221038484d1e4
Peter Lam <plam1216@gmail.com>
2023-01-19 11:36:12 -0500

    getSisters info from backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router';
 import './App.css';
 import AddJob from './Pages/AddJob/AddJob';
@@ -9,18 +10,33 @@ import NewCoach from './Pages/NewCoach/NewCoach';
 import { Navigate } from 'react-router';
 
 function App() {
+  const [sisters, setSisters] = useState(null)
+
+  const getSisters = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/sisters/")
+    const data = await response.json()
+
+    setSisters(data)
+  }
+
   return (
     <div className="App">
       {/* <Profile/> */}
       {/* IF user is logged in, Navigate to appropriate dashboard else Navigate to login */}
       <Routes>
         <Route path="/" element={<Navigate to='/login' />} />
-        <Route path="/login" element={<Landing/>}/>
-        <Route path="/dashboard" element={<MemberDashboard/>}/>
-        <Route path="/findcoach" element={<FindCoach/>}/>
-        <Route path="/findcoach/add" element={<NewCoach/>}/>
-        <Route path="/jobhub" element={<JobHub/>}/>
-        <Route path="/jobhub/add" element={<AddJob/>}/>
+        <Route path="/login" element={<Landing />} />
+        <Route path="/dashboard" element={<MemberDashboard />} />
+        <Route
+          path="/findcoach"
+          element={<FindCoach
+            sisters={sisters}
+            getSisters={getSisters}
+          />}
+        />
+        <Route path="/findcoach/add" element={<NewCoach />} />
+        <Route path="/jobhub" element={<JobHub />} />
+        <Route path="/jobhub/add" element={<AddJob />} />
       </Routes>
     </div>
   );

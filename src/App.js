7d5fba00e9504a380acfcee5b20d7e29356cefb2
Peter Lam <plam1216@gmail.com>
2023-01-28 22:10:05 -0500

    filter coaches (case-sensitive)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,59 @@ import { Navigate } from 'react-router';
 
 function App() {
   const [sisters, setSisters] = useState(null)
+  console.log("Sisters", sisters)
+
+  // state for updated sisters
+  const [filteredSisters, setFilteredSisters] = useState(sisters)
+  console.log("Filtered Sisters", filteredSisters)
+
+  const onSisterFilterSubmit = (filter) => {
+    if (filter.position === '' && filter.company === '' && filter.location === '') {
+      setFilteredSisters(sisters)
+    }
+
+    if (filter.position) {
+      let positionFilter = sisters.filter(sister => sister.current_position_txt.includes(filter.position))
+
+      if (filter.company) {
+        let companyFilter = positionFilter.filter(sister => sister.current_company_txt.includes(filter.company))
+
+        if (filter.location) {
+          let locationFilter = companyFilter.filter(sister => sister.current_city_txt.includes(filter.location))
+
+          setFilteredSisters(locationFilter)
+          return
+        }
+
+        setFilteredSisters(companyFilter)
+      }
+      else if (filter.location) {
+        let locationFilter = positionFilter.filter(sister => sister.current_city_txt.includes(filter.location))
+        console.log('Current City Filter', locationFilter)
+
+        setFilteredSisters(locationFilter)
+        return
+      }
+      
+      setFilteredSisters(positionFilter)
+    }
+    else if (filter.company) {
+      let companyFilter = sisters.filter(sister => sister.current_company_txt.includes(filter.company))
+
+      if (filter.location) {
+        let locationFilter = companyFilter.filter(sister => sister.current_city_txt.includes(filter.location))
+
+        setFilteredSisters(locationFilter)
+      }
+      
+      setFilteredSisters(companyFilter)
+    }
+    else if (filter.location) {
+      let locationFilter = sisters.filter(sister => sister.current_city_txt.includes(filter.location))
+
+      setFilteredSisters(locationFilter)
+    }
+  }
 
   const getSisters = async () => {
     const response = await fetch("http://127.0.0.1:8000/api/sisters/")
@@ -32,6 +85,7 @@ function App() {
           element={<FindCoach
             sisters={sisters}
             getSisters={getSisters}
+            onSisterFilterSubmit={onSisterFilterSubmit}
           />}
         />
         <Route path="/findcoach/add" element={<NewCoach />} />
--- src/Pages/FindCoach/FindCoach.js
+++ src/Pages/FindCoach/FindCoach.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "../../Components/Banner/Banner";
 import NavBar from "../../Components/NavBar/NavBar";
 import CoachArea from "./CoachArea/CoachArea";
-import Filters from "./Filters/Filters";
+import CoachFilter from "./CoachFilter/CoachFilter";
 
-const FindCoach = ({ sisters, getSisters }) => {
+const FindCoach = ({ sisters, getSisters, onSisterFilterSubmit }) => {
+    const [filter, setFilter] = useState({
+        position: '',
+        company: '',
+        location: '',
+    })
+
+    const handleFilterChange = (event) => {
+        console.log(event.target.value)
+        setFilter({ ...filter, [event.target.name]: event.target.value })
+    }
+
+    const handleFilterSubmit = (event) => {
+        event.preventDefault()
+        // console.log("filter", filter)
+        onSisterFilterSubmit(filter)
+    }
+
+    console.log(filter)
     return (
         <div>
             <NavBar />
             <Banner bannerText={"Find a Coach"} />
-            <Filters></Filters>
+            <CoachFilter
+                handleFilterChange={handleFilterChange}
+                handleFilterSubmit={handleFilterSubmit}
+            />
             <CoachArea
                 sisters={sisters}
                 getSisters={getSisters}

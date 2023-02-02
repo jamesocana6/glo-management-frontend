eba5fecacb36cbae75a2f91e8e83a5b67a696006
Peter Lam <plam1216@gmail.com>
2023-02-02 10:58:41 -0500

    add Profile link and KPL logo

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+import { Person } from "react-bootstrap-icons";
+
 import "./style.css";
+import kplLogo from "../../assets/kplLogo.png"
 
 const NavBar = ({ props }) => {
     const [chapters, setChapters] = useState(null)
@@ -17,12 +21,12 @@ const NavBar = ({ props }) => {
         getChapters()
     }, [])
 
-    console.log(chapters)
+    // console.log(chapters)
 
     const loaded = () => {
         let allChapters = chapters.map((chapter) => {
             return (
-                <li><a className="dropdown-item" href="#">{chapter.chapter_school}</a></li>
+                <li><a className="dropdown-item" href="#">{chapter.chapter_school_txt}</a></li>
             )
         })
 
@@ -41,58 +45,65 @@ const NavBar = ({ props }) => {
         )
     }
 
-return (
-    <div>
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="./dashboard">Navbar</a>
-                <div>
-                    <ul className="navbar-nav justify-content-end">
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                My Chapter
-                            </a>
-                            <ul className="dropdown-menu">
-                                <Link className="dropdown-item" to={"/dashboard"}>Dashboard</Link>
-                                <Link className="dropdown-item" to={"/findcoach"}>Find a Coach</Link>
-                                <Link className="dropdown-item" to={"/jobhub"}>Job Hub</Link>
-                            </ul>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Nationals
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Chapters
-                            </a>
-                            {chapters ? loaded() : loading()}
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                All Events
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Profile</a>
-                        </li>
-                    </ul>
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg" style={{padding: '2px'}}>
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="./dashboard">
+                        <img src={kplLogo} style={{width: '110px'}}/>
+                    </a>
+                    <div>
+                        <ul className="navbar-nav justify-content-end">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    My Chapter
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <Link className="dropdown-item" to={"/dashboard"}>Dashboard</Link>
+                                    <Link className="dropdown-item" to={"/findcoach"}>Find a Coach</Link>
+                                    <Link className="dropdown-item" to={"/jobhub"}>Job Hub</Link>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Nationals
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+
+                            {/* Chapters Dropdown */}
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Chapters
+                                </a>
+                                {chapters ? loaded() : loading()}
+                            </li>
+
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    All Events
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+
+                            {/* Profile */}
+                            <li className="nav-item">
+                                <Link className="nav-link" to={"/profile"}><Person size={25} /></Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </div>
-)
+            </nav>
+        </div>
+    )
 }
 
 export default NavBar;

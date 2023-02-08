eba7029c02f5152638ceb2a1d0d7d76ffe8feae9
Peter Lam <plam1216@gmail.com>
2023-02-07 19:21:47 -0500

    redirect to Login page if not logged in

--- src/Pages/MemberDashboard/MemberDashboard.js
+++ src/Pages/MemberDashboard/MemberDashboard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
 import Announcements from "../../Components/Announcements/Annoucements";
 import MemberInfo from "./MemberInfo/MemberInfo"
@@ -7,46 +8,52 @@ import NavBar from "../../Components/NavBar/NavBar";
 
 
 const MemberDashboard = ({ props }) => {
-    return (
-        <>
-            <div>
-                <NavBar />
-                <h1>MemberDashboard Page</h1>
-                Hi First Name!
-            </div>
+    const loggedInUser = localStorage.getItem("authenticated");
 
-            <div className="container-fluid">
-                <div className="row m-3" style={{ height: '17rem' }}>
-                    <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
-                        <MemberInfo />
-                    </div>
-
-                    <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
-                        <Announcements />
-                        {/* props = chapter */}
-                    </div>
+    if (!loggedInUser) {
+        return <Navigate replace to="/login" />;
+    } else {
+        return (
+            <>
+                <div>
+                    <NavBar />
+                    <h1>MemberDashboard Page</h1>
+                    Hi First Name!
                 </div>
 
-                <div className="row m-3" style={{ height: '17rem' }}>
-                    <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
-                        <p>
-                            <h1>National Resources</h1>
-                            <Link to="../findcoach">Find a Coach</Link><br></br>
-                            <Link to="../jobhub">Job Hub - Opps & Resources</Link><br></br>
-                            Support other Sister's Businesses<br></br>
-                        </p>
-                    </div>
+                <div className="container-fluid">
+                    <div className="row m-3" style={{ height: '17rem' }}>
+                        <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
+                            <MemberInfo />
+                        </div>
 
-                    <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
-                        <p>
+                        <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
                             <Announcements />
-                            {/* props = national */}
-                        </p>
+                            {/* props = chapter */}
+                        </div>
+                    </div>
+
+                    <div className="row m-3" style={{ height: '17rem' }}>
+                        <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
+                            <p>
+                                <h1>National Resources</h1>
+                                <Link to="../findcoach">Find a Coach</Link><br></br>
+                                <Link to="../jobhub">Job Hub - Opps & Resources</Link><br></br>
+                                Support other Sister's Businesses<br></br>
+                            </p>
+                        </div>
+
+                        <div className="col m-3" style={{ backgroundColor: 'lightgrey' }}>
+                            <p>
+                                <Announcements />
+                                {/* props = national */}
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        );
+    }
 }
 
 export default MemberDashboard;
